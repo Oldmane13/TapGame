8811bd36e5a41e5117fae1f2eb0b9072a5358415
OLDMANE\Oldmane <alexbatrinu13@yahoo.com>
2017-04-27 13:22:10 +0100

    Initial upload of project with swipe and tap detection scripts

---
+++ Assets/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public enum States {touch, touchHold, swipeLeft, swipeRight, doubleTap};
+	public States currentState;
+
+	public SwipeManager swipeManager;
+
+
+
+	void Start () {
+		swipeManager = FindObjectOfType <SwipeManager> ();
+		currentState = States.swipeLeft;
+	}
+	
+
+	void Update () {
+
+		if (currentState == States.swipeLeft) {
+
+			if(swipeManager.IsSwiping (SwipeDirection.Left)) {
+				Debug.Log ("Left");
+				currentState = States.swipeRight;
+			} else {
+				Debug.Log ("no");
+			}
+		}
+
+		if (currentState == States.swipeRight){
+			if (swipeManager.IsSwiping (SwipeDirection.Right)) {
+				Debug.Log ("Right");
+				currentState = States.touch;
+			} else {
+				Debug.Log ("no");
+			}
+		}
+
+		if (currentState == States.touch) {
+			if (Input.GetMouseButtonDown (0)) {
+				Debug.Log ("touch");
+				currentState = States.touchHold;
+			}
+		}
+
+
+	}
+}
---
+++ Assets/SwipeManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SwipeDirection
+{
+	None = 0,
+	Left = 1,
+	Right = 2,
+	Up = 4,
+	Down = 8,
+}
+
+public class SwipeManager : MonoBehaviour {
+
+	public SwipeDirection Direction{set;get;}
+
+	private Vector3 touchPosition;
+	private float swipeResistanceX = 50.0f;
+	private float swipeResistanceY = 100.0f;
+
+	private void Update(){
+
+		Direction = SwipeDirection.None;
+
+		if (Input.GetMouseButtonDown (0)) {
+			touchPosition = Input.mousePosition;
+		}
+
+		if (Input.GetMouseButtonUp (0)) {
+			Vector2 deltaSwipe = touchPosition - Input.mousePosition;
+
+			if (Mathf.Abs (deltaSwipe.x) > swipeResistanceX) {
+			
+				//Swipe on the X Axis
+				Direction |= (deltaSwipe.x < 0 ? SwipeDirection.Right : SwipeDirection.Left);
+			}
+
+			if (Mathf.Abs (deltaSwipe.y) > swipeResistanceY) {
+
+				//Swipe on the Y Axis
+				Direction |= (deltaSwipe.y < 0 ? SwipeDirection.Up : SwipeDirection.Down);
+			}
+		}
+
+		
+	}
+	public bool IsSwiping (SwipeDirection dir){
+		return(Direction & dir) == dir;
+	}
+}
